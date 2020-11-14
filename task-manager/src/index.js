26c2b93fb520178b5d829758dde264db5bb0b77b
Ahmet Ali Özdemir <ahmetali131011@gmail.com>
2020-11-14 07:47:30 +0300

    Use user virtual,

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -31,11 +31,15 @@ app.listen(port, () => {
 });
 
 const Task = require('./models/task');
+const User = require('./models/user');
 
 const main = async () => {
-  const task = await Task.findById('5faf5bb45da4784e9477568a')
-  await task.populate('owner').execPopulate()
-  console.log(task.owner);
+  const user = await User.findById('5faf5af0a4c5644bf4f835a4');
+  await user.populate('tasks').execPopulate()
+  console.log(user.tasks);
+  // const task = await Task.findById('5faf5bb45da4784e9477568a')
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner);
 }
 
 main()
